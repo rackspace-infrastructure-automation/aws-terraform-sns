cb21226859b978fa58e731dc8c3c3ae40946148a
Steven Gorrell <stevengor@gmail.com>
2018-09-06 08:19:48 -0500

    Added two more subscriptions to match CFN templates.

--- main.tf
+++ main.tf
@@ -22,3 +22,17 @@ resource "aws_sns_topic_subscription" "Subscription3" {
   protocol  = "${var.protocol_3}"
   endpoint  = "${var.endpoint_3}"
 }
+
+resource "aws_sns_topic_subscription" "Subscription4" {
+  count     = "${var.create_subscription_4}"
+  topic_arn = "${aws_sns_topic.MySNSTopic.arn}"
+  protocol  = "${var.protocol_4}"
+  endpoint  = "${var.endpoint_4}"
+}
+
+resource "aws_sns_topic_subscription" "Subscription5" {
+  count     = "${var.create_subscription_5}"
+  topic_arn = "${aws_sns_topic.MySNSTopic.arn}"
+  protocol  = "${var.protocol_5}"
+  endpoint  = "${var.endpoint_5}"
+}
--- variables.tf
+++ variables.tf
@@ -11,7 +11,7 @@ variable "create_subscription_1" {
 }
 
 variable "protocol_1" {
-  # Terraform does not currently support email as a protocol. See: https://www.terraform.io/docs/providers/aws/r/sns_topic_subscription.html
+  # Terraform does not currently support email or email-json as a protocol. See: https://www.terraform.io/docs/providers/aws/r/sns_topic_subscription.html
   description = "The protocol you want to use in your endpoint #1. Supported protocols include: http, https, sms, sqs, application, lambda."
   type        = "string"
   default     = ""
@@ -33,8 +33,8 @@ variable "create_subscription_2" {
 }
 
 variable "protocol_2" {
-  # Terraform does not currently support email as a protocol. See: https://www.terraform.io/docs/providers/aws/r/sns_topic_subscription.html
-  description = "The protocol you want to use in your endpoint #2. Supported protocols include: http, https, email, email-json, sms, sqs, application, lambda."
+  # Terraform does not currently support email or email-json as a protocol. See: https://www.terraform.io/docs/providers/aws/r/sns_topic_subscription.html
+  description = "The protocol you want to use in your endpoint #2. Supported protocols include: http, https, sms, sqs, application, lambda."
   type        = "string"
   default     = ""
 }
@@ -55,14 +55,58 @@ variable "create_subscription_3" {
 }
 
 variable "protocol_3" {
-  # Terraform does not currently support email as a protocol. See: https://www.terraform.io/docs/providers/aws/r/sns_topic_subscription.html
-  description = "The protocol you want to use in your endpoint #3. Supported protocols include: http, https, email, email-json, sms, sqs, application, lambda."
+  # Terraform does not currently support email or email-json as a protocol. See: https://www.terraform.io/docs/providers/aws/r/sns_topic_subscription.html
+  description = "The protocol you want to use in your endpoint #3. Supported protocols include: http, https, sms, sqs, application, lambda."
   type        = "string"
   default     = ""
 }
 
 variable "endpoint_3" {
   description = "The subscription's endpoint #3."
+  type        = "string"
+  default     = ""
+}
+
+# #####################################
+
+# Subscription 4
+variable "create_subscription_4" {
+  description = "Set to true to enable subscription."
+  type        = "string"
+  default     = false
+}
+
+variable "protocol_4" {
+  # Terraform does not currently support email or email-json as a protocol. See: https://www.terraform.io/docs/providers/aws/r/sns_topic_subscription.html
+  description = "The protocol you want to use in your endpoint #4. Supported protocols include: http, https, sms, sqs, application, lambda."
+  type        = "string"
+  default     = ""
+}
+
+variable "endpoint_4" {
+  description = "The subscription's endpoint #4."
+  type        = "string"
+  default     = ""
+}
+
+# #####################################
+
+# Subscription 5
+variable "create_subscription_5" {
+  description = "Set to true to enable subscription."
+  type        = "string"
+  default     = false
+}
+
+variable "protocol_5" {
+  # Terraform does not currently support email or email-json as a protocol. See: https://www.terraform.io/docs/providers/aws/r/sns_topic_subscription.html
+  description = "The protocol you want to use in your endpoint #5. Supported protocols include: http, https, sms, sqs, application, lambda."
+  type        = "string"
+  default     = ""
+}
+
+variable "endpoint_5" {
+  description = "The subscription's endpoint #5."
   type        = "string"
   default     = ""
 }
