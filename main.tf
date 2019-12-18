e4c41dadbf3e49bdaf52d8f2031b89e77d1f0eef
John Titus <john.titus@rackspace.com>
2019-12-18 14:26:59 -0600

    Correct conversion errors

--- main.tf
+++ main.tf
@@ -11,35 +11,35 @@ resource "aws_sns_topic" "MySNSTopic" {
 }
 
 resource "aws_sns_topic_subscription" "Subscription1" {
-  count     = var.create_subscription_1
+  count     = var.create_subscription_1 ? 1 : 0
   topic_arn = aws_sns_topic.MySNSTopic.arn
   protocol  = var.protocol_1
   endpoint  = var.endpoint_1
 }
 
 resource "aws_sns_topic_subscription" "Subscription2" {
-  count     = var.create_subscription_2
+  count     = var.create_subscription_2 ? 1 : 0
   topic_arn = aws_sns_topic.MySNSTopic.arn
   protocol  = var.protocol_2
   endpoint  = var.endpoint_2
 }
 
 resource "aws_sns_topic_subscription" "Subscription3" {
-  count     = var.create_subscription_3
+  count     = var.create_subscription_3 ? 1 : 0
   topic_arn = aws_sns_topic.MySNSTopic.arn
   protocol  = var.protocol_3
   endpoint  = var.endpoint_3
 }
 
 resource "aws_sns_topic_subscription" "Subscription4" {
-  count     = var.create_subscription_4
+  count     = var.create_subscription_4 ? 1 : 0
   topic_arn = aws_sns_topic.MySNSTopic.arn
   protocol  = var.protocol_4
   endpoint  = var.endpoint_4
 }
 
 resource "aws_sns_topic_subscription" "Subscription5" {
-  count     = var.create_subscription_5
+  count     = var.create_subscription_5 ? 1 : 0
   topic_arn = aws_sns_topic.MySNSTopic.arn
   protocol  = var.protocol_5
   endpoint  = var.endpoint_5
