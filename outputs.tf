979ae8e04c886fa46d8cac981d6bdb06db4f5f00
John Titus <john.titus@rackspace.com>
2019-12-18 15:17:43 -0600

    Update variable and resource names to match style guide

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "topic_arn" {
   description = "The ARN of the SNS Topic"
-  value       = aws_sns_topic.MySNSTopic.arn
+  value       = aws_sns_topic.topic.arn
 }
 
 output "topic_id" {
   description = "The id of the SNS topic."
-  value       = aws_sns_topic.MySNSTopic.id
+  value       = aws_sns_topic.topic.id
 }
