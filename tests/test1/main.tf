88643c0ff1c229cba6bc0c405a39f218fb9eda3f
John Titus <john.titus@rackspace.com>
2020-01-23 11:13:52 -0600

    Test updates to resolve failures

--- tests/test1/main.tf
+++ tests/test1/main.tf
@@ -7,20 +7,20 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "random_string" "sqs_rstring" {
+resource "random_string" "rstring" {
   length  = 18
   upper   = false
   special = false
 }
 
 resource "aws_sqs_queue" "my_sqs" {
-  name = "${random_string.sqs_rstring.result}-my-example-queue"
+  name = "${random_string.rstring.result}-my-example-queue"
 }
 
 module "sns_sqs" {
   source = "../../module"
 
-  name = "${random_string.sqs_rstring.result}-my-example-topic"
+  name = "${random_string.rstring.result}-my-example-topic"
 
   create_subscription_1 = true
   endpoint_1            = aws_sqs_queue.my_sqs.arn
